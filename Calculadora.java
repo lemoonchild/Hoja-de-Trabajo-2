46895fdb2f50fabf3e87d88a06bd457dc836594f
Fabiola Contreras <98127191+Fabiola-cc@users.noreply.github.com>
2023-01-28 12:24:22 -0600

    Modificación de métodos count, isEmpty & getItems

--- Calculadora.java
+++ Calculadora.java
@@ -1,8 +1,12 @@
 import java.util.ArrayList;
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Arrays;
+import java.util.Scanner; // Import the Scanner class to read text files
 
 /**
  * @author 
- * Maria José Villafuerte 
+ * Maria José Villafuerte
  * Fabiola Contreras
  * Madeline Castro 
  * @category Ejercicio #1 
@@ -10,7 +14,24 @@ import java.util.ArrayList;
  * @date 30/01/2023
  */
 
-public class Calculadora implements IPostfixCalculator, IStack{
+public class Calculadora<T> implements IPostfixCalculator, IStack<T>{
+    private ArrayList<String> op = new ArrayList<String>(); //Guarda todas las operaciones necesarias en posfix
+    private ArrayList<T> ourStack = new ArrayList<T>();
+
+    private void ReadDoc (String doc) { //Lee el documento con operaciones y guarda los datos
+        try {
+            File data = new File(doc);
+            Scanner myReader = new Scanner(data);
+            while (myReader.hasNextLine()) {
+                String info = myReader.nextLine();
+                op.add(info);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public boolean isOneItem(IStack operandos) {
@@ -49,36 +70,35 @@ public class Calculadora implements IPostfixCalculator, IStack{
 
     @Override
     public ArrayList<String> getItems(String _expresion) {
-        // TODO Auto-generated method stub
-        return null;
+        String[] elements = _expresion.split(" "); //Guarda en una lista cada elemento de la expresión omitiendo los espacios en blanco
+        return new ArrayList<String>(Arrays.asList(elements)); //Retorna la lista convertida en un ArrayList
     }
+
 
     @Override
     public int count() {
-        // TODO Auto-generated method stub
-        return 0;
+        return ourStack.size();
     }
 
     @Override
     public boolean isEmpty() {
-        // TODO Auto-generated method stub
-        return false;
+        return ourStack.isEmpty();
     }
 
     @Override
-    public void push(Object value) {
+    public void push(T value) {
         // TODO Auto-generated method stub
         
     }
 
     @Override
-    public Object pull() {
+    public T pull() {
         // TODO Auto-generated method stub
         return null;
     }
 
     @Override
-    public Object peek() {
+    public T peek() {
         // TODO Auto-generated method stub
         return null;
     }
